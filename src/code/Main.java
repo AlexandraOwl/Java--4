76cc0cdd695a944f66813b33f36fe7e4fb919ed8
AlexandraOwl <nikolachuk98@mail.ru>
2021-04-23 08:52:19 +0300

    Новый файл

---
+++ src/code/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long ticket = 4000;
+        int bonus = 20;
+        long totalbonus = ticket / bonus;
+        System.out.println(totalbonus);
+    }
+}
